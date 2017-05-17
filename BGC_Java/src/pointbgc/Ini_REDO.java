0fe834ebb8ffede245523cb3958558c5c09e26d6
Odzinic <Odzinic@gmail.com>
2017-05-17 16:19:50 -0400

    - Created a new class for returning values from scan_value - Changed scan_value calls in Restart_Ini

--- BGC_Java/src/pointbgc/Ini_REDO.java
+++ BGC_Java/src/pointbgc/Ini_REDO.java
@@ -7,6 +7,26 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+
+
+/* ReturnValue is a class that is used for returning multiple data types
+   when scan_value is called. Can return a String, Integer or Double.
+ */
+class ReturnValue{
+
+    public String strVal = null;
+    // TODO See if -1 is a good initialization value
+    public int intVal = -1;
+    public double doubVal = -1.0;
+
+//    public ReturnValue(String strVal, int intVal, double doubVal){
+    public ReturnValue(){
+
+    }
+
+}
+
+
 public class Ini_REDO {
 
     public File init;
@@ -76,6 +96,74 @@ public class Ini_REDO {
         sc.close();
         return readLines;
 
+    }
+
+    /* scan_value receives an integer iPos and char cType that determines which value from inputLines
+       will be returned and what data type it will be stored as. Possible values for type 'i' for integer
+       'd' for double 's' for string
+    */
+    ReturnValue scan_value(int iPos, char cType){
+
+
+        // Check if the specified list position is in the bounds of the list. If false, returns null.
+        if (iPos >= inputLines.size()){
+
+            System.out.println("ERROR: Specified position value iPos is outside the the list bounds.");
+
+            return null;
+        }
+
+        switch (cType){
+
+            case 'i':
+                ReturnValue returnInt = new ReturnValue();
+
+                try{
+
+                    returnInt.intVal = Integer.valueOf(inputLines.get(iPos));
+//                    readInt.add(Integer.valueOf(inputLines.get(iPos)));
+
+                    return returnInt;
+                } catch (UnsupportedOperationException uoe) {
+
+                    uoe.printStackTrace();
+                    return null;
+                }
+
+            case 'd':
+                ReturnValue returnDouble = new ReturnValue();
+
+                try{
+
+                    returnDouble.doubVal = Double.valueOf(inputLines.get(iPos));
+//                    readDouble.add(Double.valueOf(inputLines.get(iPos)));
+
+                    return returnDouble;
+                } catch (UnsupportedOperationException uoe) {
+
+                    uoe.printStackTrace();;
+                    return null;
+                }
+
+            case 's':
+                ReturnValue returnString = new ReturnValue();
+
+                try{
+
+                    returnString.strVal = String.valueOf(inputLines.get(iPos));
+//                    readString.add(String.valueOf(inputLines.get(iPos)));
+                } catch (UnsupportedOperationException uoe) {
+
+                    uoe.printStackTrace();
+                    return null;
+                }
+
+            default:
+
+                System.out.println("ERROR: Unsupported data type specified for scan_value.");
+        }
+
+        return null;
     }
 
 
--- BGC_Java/src/pointbgc/Restart_Init.java
+++ BGC_Java/src/pointbgc/Restart_Init.java
@@ -3,12 +3,10 @@ package pointbgc;
 
 import classes.Constant;
 import classes.Restart_Ctrl;
-
 import java.io.File;
 
 /*
 restart_init.c
-Initialize the simulation restart parameters
 
 *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 Biome-BGC version 4.2 (final release)
@@ -24,12 +22,12 @@ public class Restart_Init {
 
         double cli_mode = 0.0; // TODO NEED TO CREATE PUBLIC STATIC CLI_MODE IN POINTBGC. Should be a double.
 
-        String BV_ERROR= String.valueOf(Constant.BV_ERROR.getValue());
+        String BV_ERROR = String.valueOf(Constant.BV_ERROR.getValue());
         double MODE_SPINUP = Constant.MODE_SPINUP.getValue();
         double MODE_SPINNGO = Constant.MODE_SPINNGO.getValue();
         double MODE_MODEL = Constant.MODE_MODEL.getValue();
 
-        Ini ini = new Ini();
+        Ini_REDO ini = new Ini_REDO(init, key1);
 
         /********************************************************************
          **                                                                 **
@@ -38,18 +36,11 @@ public class Restart_Init {
          **                                                                 **
          ********************************************************************/
 
-        /* scan for the restart block keyword, exit if not next */
-        keyword = String.valueOf(ini.scan_value(init, 's').get(0));
-        if (!keyword.equals(key1)){
-
-            System.out.printf(BV_ERROR, "Error reading keyword for restart data\n");
-            return 0;
-        }
 
         /* check for input restart file */
         // TODO Make ini.scan_value return null if value isn't found BUT CANNOT RETURN NULL FOR INT
         // TODO Set an error code for when scanning for an int. Currently set as -1
-        restart.read_restart = ini.scan_value(init, 'i').get(0);
+        restart.read_restart = ini.scan_value(0, 'i').intVal;
         if (restart.read_restart  == -1){
 
             System.out.printf(BV_ERROR, "Error reading input restart flag\n");
@@ -58,16 +49,26 @@ public class Restart_Init {
         }
 
         if (cli_mode == MODE_SPINUP || cli_mode == MODE_SPINNGO){
-
             restart.read_restart = 0;
-
         }
-
-        else if (cli_mode == MODE_MODEL){
-
+        if (cli_mode == MODE_MODEL){
             restart.read_restart = 1;
-
         }
+
+        /* check for output restart file */
+        restart.write_restart = ini.scan_value(1, 'i').intVal;
+
+        if (cli_mode == MODE_SPINUP){
+            restart.write_restart = 1;
+        }
+        if (cli_mode == MODE_MODEL || cli_mode == MODE_SPINNGO){
+            restart.write_restart = 0;
+        }
+
+        /* flag for metyear handling */
+        restart.keep_metyr = ini.scan_value(2, 'i').intVal;
+
+
 
 
 
